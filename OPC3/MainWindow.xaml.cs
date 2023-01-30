c8dd18e5b8b026ba593051bee1dd7b32350b8103
Alex Malik <alex.malik@outlook.com>
2023-01-30 21:56:31 +0100

    Add project files.

---
+++ OPC3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace OPC3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random _random = new Random();
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            double left = _random.NextDouble() * (ActualWidth - button.ActualWidth);
+            double top = _random.NextDouble() * (ActualHeight - button.ActualHeight);
+            button.Margin = new Thickness(left, top, 0, 0);
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+
+    }
+}
+
